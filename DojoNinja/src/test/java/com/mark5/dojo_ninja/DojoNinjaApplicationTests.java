c8ac04424a2986f38b106e5deb5b310a44c4445c
mholdenmd <77797864+mholdenmd@users.noreply.github.com>
2021-09-13 15:14:36 -0400

    Add files via upload

---
+++ DojoNinja/src/test/java/com/mark5/dojo_ninja/DojoNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark5.dojo_ninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
